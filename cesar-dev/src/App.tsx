3978dca80f887c1c3bdf187cd612114f3e06476f
Cesar <cesaragapito29@gmail.com>
2024-06-28 00:42:26 -0500

    Version 1.1

--- cesar-dev/src/App.tsx
+++ cesar-dev/src/App.tsx
@@ -1,34 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <>
+          <nav className="navbar navbar-expand-lg navbar-light bg-body-tertiary fixed-top">
+              <div className="container">
+                  <a className="navbar-brand" href="#">
+                      <img src="https://react-dev-five-xi.vercel.app/vite.svg" alt="React Dev"/>
+                  </a>
+                  <button
+                      data-mdb-collapse-init
+                      className="navbar-toggler"
+                      type="button"
+                      data-mdb-target="#navbarTogglerDemo01"
+                      aria-controls="navbarTogglerDemo01"
+                      aria-expanded="false"
+                      aria-label="Toggle navigation"
+                  >
+                      <i className="fas fa-bars"></i>
+                  </button>
+                  <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+
+                      <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                          <li className="nav-item">
+                              <a className="nav-link active" aria-current="page" href="#">INICIO</a>
+                          </li>
+                          <li className="nav-item">
+                              <a className="nav-link" href="#">SERVICIOS</a>
+                          </li>
+                          <li className="nav-item">
+                              <a className="nav-link" href="#">PORTAFOLIO</a>
+                          </li>
+                          <li className="nav-item">
+                              <a className="nav-link" href="#">CONTACTO</a>
+                          </li>
+                          <li className="nav-item">
+                              <a href="#" className="btn btn-outline-dark ms-2">CONTACTO</a>
+                          </li>
+                      </ul>
+                  </div>
+              </div>
+          </nav>
+      </>
   )
 }
 
